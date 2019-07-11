e62f355c684f59d62f79c6e1e067d98a280ed1e3
정성윤 <mznx13579@cau.ac.kr>
2019-07-11 16:47:41 +0900

    alpha-beta pruning

--- Gomoku Game Project/Client/MenuForm.Designer.cs
+++ Gomoku Game Project/Client/MenuForm.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.singlePlayButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.multiPlayButton = new System.Windows.Forms.Button();
+            this.AIPlayButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // singlePlayButton
             // 
-            this.singlePlayButton.Location = new System.Drawing.Point(250, 106);
+            this.singlePlayButton.Location = new System.Drawing.Point(250, 97);
             this.singlePlayButton.Name = "singlePlayButton";
             this.singlePlayButton.Size = new System.Drawing.Size(100, 40);
             this.singlePlayButton.TabIndex = 0;
@@ -45,7 +48,7 @@
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(250, 198);
+            this.exitButton.Location = new System.Drawing.Point(250, 235);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(100, 40);
             this.exitButton.TabIndex = 1;
@@ -55,7 +58,7 @@
             // 
             // multiPlayButton
             // 
-            this.multiPlayButton.Location = new System.Drawing.Point(250, 152);
+            this.multiPlayButton.Location = new System.Drawing.Point(250, 143);
             this.multiPlayButton.Name = "multiPlayButton";
             this.multiPlayButton.Size = new System.Drawing.Size(100, 40);
             this.multiPlayButton.TabIndex = 2;
@@ -63,11 +66,27 @@
             this.multiPlayButton.UseVisualStyleBackColor = true;
             this.multiPlayButton.Click += new System.EventHandler(this.MultiPlayButton_Click);
             // 
+            // AIPlayButton
+            // 
+            this.AIPlayButton.Location = new System.Drawing.Point(250, 189);
+            this.AIPlayButton.Name = "AIPlayButton";
+            this.AIPlayButton.Size = new System.Drawing.Size(100, 40);
+            this.AIPlayButton.TabIndex = 3;
+            this.AIPlayButton.Text = "인공지능 연습";
+            this.AIPlayButton.UseVisualStyleBackColor = true;
+            this.AIPlayButton.Click += new System.EventHandler(this.AIPlayButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.AIPlayButton);
             this.Controls.Add(this.multiPlayButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.singlePlayButton);
@@ -82,6 +101,8 @@
         private System.Windows.Forms.Button singlePlayButton;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Button multiPlayButton;
+        private System.Windows.Forms.Button AIPlayButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- Gomoku Game Project/Client/MenuForm.cs
+++ Gomoku Game Project/Client/MenuForm.cs
@@ -41,5 +41,13 @@ namespace Client
             multiPlayForm.FormClosed += new FormClosedEventHandler(childForm_Closed);
             multiPlayForm.Show();
         }
+
+        private void AIPlayButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            AIPlayForm aiPlayform = new AIPlayForm();
+            aiPlayform.FormClosed += new FormClosedEventHandler(childForm_Closed);
+            aiPlayform.Show();
+        }
     }
 }
